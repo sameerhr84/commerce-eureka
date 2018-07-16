e00ee3d33117351e5443bd56adb43be30f84365c
sameer <sameer920@gmail.com>
2018-07-16 11:01:56 -0500

    eureka

--- src/main/java/com/retail/ecom/eureka/bootstrap/EurekaServerApplication.java
+++ src/main/java/com/retail/ecom/eureka/bootstrap/EurekaServerApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 	
 
-	public static void main(String args)
+	public static void main(String[] args)
 	{
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
