348bb6852c8a272ae8714944804113dca4902b74
sameer <sameer920@gmail.com>
2018-07-15 22:43:02 -0500

    eureka initital check in

---
+++ src/main/java/com/retail/ecom/eureka/bootstrap/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.retail.ecom.eureka.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+
+@SpringBootApplication(scanBasePackages={"com.retail"})
+@EnableEurekaServer
+public class EurekaServerApplication {
+	
+
+	public static void main(String args)
+	{
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
